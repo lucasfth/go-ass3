73e9513332d5801e2eb7467b574f80d59c15dd71
Lucas Hanson <94063597+Lucasfth@users.noreply.github.com>
2022-10-31 20:52:30 +0100

    Imp small requirements (#5)
    * add max length to messages
    
    * fix join message
    
    * fix leave message

--- client/client.go
+++ client/client.go
@@ -58,7 +58,7 @@ func joinChat(ctx context.Context, client chittyChat.ChatServiceClient, username
 		log.Println("------------------------------")
 	}
 
-	sendMessage(ctx, client, "was added to the chat", username)
+	sendMessage(ctx, client, "joined Chitty-Chat", username)
 
 	waitc := make(chan struct{})
 	go func() {
@@ -70,10 +70,10 @@ func joinChat(ctx context.Context, client chittyChat.ChatServiceClient, username
 			if username != messageIncoming.User {
 				lamportTimeStamp = int64(math.Max(float64(messageIncoming.LamportTimeStamp), float64(lamportTimeStamp)))
 				LPTS := strconv.FormatInt(lamportTimeStamp, 10)
-				if messageIncoming.Message == "was added to the chat" {
-					log.Println("-", messageIncoming.User, messageIncoming.Message, "at Lamport time", LPTS, "-")
-				} else if messageIncoming.Message == "left chat" {
-					log.Println("-", messageIncoming.User, messageIncoming.Message, "at Lamport time", LPTS, "-")
+				if messageIncoming.Message == "joined Chitty-Chat" {
+					log.Println("- Participant", messageIncoming.User, messageIncoming.Message, "at Lamport time", LPTS, "-")
+				} else if messageIncoming.Message == "left Chitty-Chat" {
+					log.Println("- Participant", messageIncoming.User, messageIncoming.Message, "at Lamport time", LPTS, "-")
 				} else {
 					log.Println("["+LPTS+"]["+messageIncoming.User+"]", messageIncoming.Message)
 				}
@@ -92,9 +92,14 @@ func leaveChat(ctx context.Context, client chittyChat.ChatServiceClient, usernam
 }
 
 func sendMessage(ctx context.Context, client chittyChat.ChatServiceClient, message string, username string) {
+	if 128 < len(message) {
+		log.Println("Message is too long. Max 128 characters")
+		return
+	}
+
 	lamportTimeStamp++
 	if message == "leave chat" {
-		message = "left chat"
+		message = "left Chitty-Chat"
 	}
 	stream, err := client.SendMessage(ctx)
 	if err != nil {
@@ -109,7 +114,7 @@ func sendMessage(ctx context.Context, client chittyChat.ChatServiceClient, messa
 		LamportTimeStamp: lamportTimeStamp,
 	}
 	stream.Send(&chatMessage)
-	if message == "left chat" {
+	if message == "left Chitty-Chat" {
 		time.Sleep(10 * time.Millisecond)
 		leaveChat(ctx, client, username)
 	}
